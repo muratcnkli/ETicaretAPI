695c48e162a9f1e625b574286c5bc8b332623fc8
Murat Ceniklioğlu <89580472+muratcnkli@users.noreply.github.com>
2023-01-25 20:46:20 +0300

    Add project files.

---
+++ Insfrastructure/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                ConfigurationManager configurationManager = new ConfigurationManager();
+               // configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../ETicaretAPI.API"));
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+                configurationManager.AddJsonFile("appsettings.json");
+
+                return configurationManager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
