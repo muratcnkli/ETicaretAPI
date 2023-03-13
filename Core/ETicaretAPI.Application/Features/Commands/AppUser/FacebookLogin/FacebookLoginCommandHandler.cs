a457a97635ff753a708ea9cfc3eee1393828357c
Murat Ceniklioğlu <89580472+muratcnkli@users.noreply.github.com>
2023-03-13 22:07:11 +0300

    13.03.2023

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using ETicaretAPI.Application.Abstractions.Token;
+using ETicaretAPI.Domain.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
+{
+	public class FacebookLoginCommandHandler : IRequestHandler<FacebookLoginCommandRequest, FacebookLoginCommandResponse>
+	{
+		readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+		readonly ITokenHandler _tokenHandler;
+		readonly HttpClient _httpClient;
+
+		public FacebookLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler, IHttpClientFactory httpClientFactory)
+		{
+			_userManager = userManager;
+			_tokenHandler = tokenHandler;
+			_httpClient = httpClientFactory.CreateClient();
+		}
+
+		public Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Presentation/ETicaretAPI.API/Controllers/UsersController.cs
+++ Presentation/ETicaretAPI.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 ﻿using ETicaretAPI.Application.Features.Commands.AppUser.CreateUser;
+using ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin;
+using ETicaretAPI.Application.Features.Commands.AppUser.GoogleLogin;
 using ETicaretAPI.Application.Features.Commands.AppUser.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -28,5 +30,19 @@ namespace ETicaretAPI.API.Controllers
 			LoginUserCommandResponse response= await _mediator.Send(loginUserCommandRequest);
 			return Ok(response);
 		}
+		[HttpPost("google-login")]
+		public async Task<IActionResult> GoogleLogin(GoogleLoginCommandRequest googleLoginCommandRequest)
+		{
+			GoogleLoginCommandResponse response=await _mediator.Send(googleLoginCommandRequest);
+			return Ok(response);
+		}
+		[HttpPost("facebook-login")]
+		public async Task<IActionResult> FacebookLogin(FacebookLoginCommandRequest facebookLoginCommandRequest)
+		{
+			FacebookLoginCommandResponse response = await _mediator.Send(facebookLoginCommandRequest);
+			return Ok(response);
+		}
+
+
 	}
 }
